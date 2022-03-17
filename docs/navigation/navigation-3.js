c42ac6855002fdf838fbd88cbe769d97f20acec4
USP <ulrich.sprick@gmx.de>
2022-03-17 18:56:04 +0100

    Recognition of URLs with hash part implemented. Home page entry added to parent list.

--- docs/navigation/navigation-3.js
+++ docs/navigation/navigation-3.js
@@ -64,23 +64,26 @@ function transitionEndHandler ( evt ) {
 	evt.stopPropagation( );
 	}
 
-export function findCurrentDocument( ) {
-	const links = navpanel.getElementsByTagName( "A" );
+function findDocumentLink( documentPath, includeHash, links ) {
+	///		Search the link list for entries that match the current document path.
+	///		If includeHash is true, the hash is included in the search. This allows
+	///		links to multiple sections in the same document in the table-of-contents.
 	for ( let i = 0 ; i < links.length ; i ++ ) {
 		const link = links[ i ];
-		if ( link.href === document.location.href ) {
+		const linkPath = link.pathname + (includeHash ? link.hash : "") ;
+		if ( linkPath === documentPath ) {
 			if ( i > 0 ) previous = links[ i - 1 ].href;
 			if ( i < links.length - 1 ) next = links[ i + 1 ].href;
 			let e = link.parentNode.parentNode.firstElementChild.querySelector( "LI>A" );
 			if ( e ) first = e.href;
 			e = link.parentNode.parentNode.lastElementChild.querySelector( "LI>A" );
 			if ( e ) last = e.href;
-			activateLinkChain( link );
-			if ( parents.length > 0 ) parent = parents[ parents.length - 1 ];
-			break;
-	} } }
+			return link ;
+	}	}	}
 
 export function activateLinkChain( e ) {
+	///		Marks the parent nodes of the current document as "active"
+	///		and builds the parent nodes list.
 	while ( (e = e.parentNode) ) {
 		if ( e.tagName === "LI" ) {
 			let link = e.querySelector( "LI>A" );
@@ -89,7 +92,19 @@ export function activateLinkChain( e ) {
 			if ( e.getAttribute( "cbc" ) === "collapsed" ) {
 				e.setAttribute( "cbc", "expanded" );
 				e.synesis.collapsibleBlocks[ 0 ].style.height = e.synesis.collapsibleBlocks[ 0 ].scrollHeight + "px";
-	} } } 
+		} } } 
+	// The current document is not a parent.
 	if (parents.length > 0) parents.pop( );
 	}
 
+export function findCurrentDocument( ) {
+	///		Identify the current document in the site navigation links
+	///		and store references to related pages.
+	const links = navpanel.getElementsByTagName( "A" );
+	let link = findDocumentLink( document.location.pathname + document.location.hash, true, links ) ;
+	if ( ! link ) link = findDocumentLink( document.location.pathname + document.location.hash, false, links ) ;
+	if ( link ) activateLinkChain( link );
+	// Add the home page to the parents list
+	if ( links.length > 0 ) parents[ 0 ] = links[ 0 ].href ;
+	parent = parents[ parents.length - 1 ];
+	}
