e786dbb37fa1c67b8f3b7414c3bae15dc73c8c82
bassmationcode <BassmatiOnCode@freenet.de>
2023-02-01 19:34:32 +0100

    .

--- docs/spreadsheet-udf.js
+++ docs/spreadsheet-udf.js
@@ -1,1 +1,7 @@
-../../spreadsheet/docs/test/spreadsheet-udf-0.js
+/*
+ *		spreadsheet-functions.js   2023-01-29   usp
+ */
+
+export function test2 ( ) { 
+	return "In time we hate that which we often fear." 
+	}
--- docs/spreadsheet-utility.js
+++ docs/spreadsheet-utility.js
@@ -1,1 +1,11 @@
-../../spreadsheet/docs/test/spreadsheet-utility-0.js
+/*
+ *		spreadsheet-utitity.js   2023-02-01   usp
+ */
+
+export const norm = element => typeof element === "string" ? document.getElementById( element ) : element ;  // Normalize returns an element reference. Accepts a reference to or the ID of an element.
+
+export const normElements = elements => {
+	// Normalizes all element references in the list.
+	for ( let i = 0 ; i < elements.length ; i ++ ) elements[ i ] = norm( elements[ i ] );
+	return elements ;
+	} ;
