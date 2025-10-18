f52c1c1a87447214dfac4c06a98c99afd71c955d
BassMati <75748577+BassMatiOnCode@users.noreply.github.com>
2025-10-18 16:04:50 +0200

    Update batch files improved

--- docs/webcat/download.bat
+++ docs/webcat/download.bat
@@ -1,4 +1,22 @@
-@rem Copies files to the development folder
-@rem download folder-name\ flags
-@rem down folder-name\file-name flags
-xcopy /d /s /b %1 ..\..\..\webcat\code\%1 %2
+@ECHO OFF
+SETLOCAL
+SET args=%*
+IF NOT DEFINED args GOTO :usage 
+SET filename=%~1
+CALL SET flags=%%args:*%1=%%
+SET src=\github\bassmationcode\inc\docs\webcat
+SET dst=\github\bassmationcode\webcat\code
+XCOPY /d /s /b%flags% "%src%\%1" "%dst%\%1"
+EXIT /b
+
+:usage
+ECHO Usage: download filename flags
+ECHO   XCopies files or folders with symlinks preserved.
+ECHO   src base: %src%
+ECHO   dst base: %dst%
+ECHO   Filenames are relative to src and dst base folders. 
+ECHO   Examples:
+ECHO   download pathbar\
+ECHO   download pathbar\*.*
+ECHO   download pathbar\pathbar-1.css
+EXIT /b
--- docs/webcat/upload.bat
+++ docs/webcat/upload.bat
@@ -1,4 +1,22 @@
-@rem Copies files from the development folder
-@rem upload folder-name\*.* flags = copies all files with confirmation
-@rem upload folder-name\file-name flags = copies one file with confirmation
-xcopy /s /d /b ..\..\..\webcat\code\%1 %1 %2
+@ECHO OFF
+SETLOCAL
+SET args=%*
+IF NOT DEFINED args GOTO :usage 
+SET filename=%~1
+CALL SET flags=%%args:*%1=%%
+SET src=\github\bassmationcode\webcat\code
+SET dst=\github\bassmationcode\inc\docs\webcat
+XCOPY /d /s /b%flags% "%src%\%1" "%dst%\%1"
+EXIT /b
+
+:usage
+ECHO Usage: download filename flags
+ECHO   XCopies files or folders with symlinks preserved.
+ECHO   src base: %src%
+ECHO   dst base: %dst%
+ECHO   Filenames are relative to src and dst base folders. 
+ECHO   Examples:
+ECHO   download pathbar\
+ECHO   download pathbar\*.*
+ECHO   download pathbar\pathbar-1.css
+EXIT /b
